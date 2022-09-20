3bf43b341da8b03e3c68de33cbdfd494d29eaa64
Guilherme Braun <guiabraun@hotmail.com>
2022-09-20 20:47:53 -0300

    header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image"
+import LogoOusar from '../../public/images/logo-ousar.png'
+
 export const Header = () => {
     return (
-        <div className="absolute text-white">
-            Teste
-        </div>
+        <div className="relative container">
+            <div className="absolute text-white grid grid-cols-2 w-full container py-4">
+                    <div>
+                        <Image src={LogoOusar} width={106} height={17} />
+                    </div>
+                    <nav>
+                        <ul className="flex justify-end gap-x-4">
+                            <li>...</li>
+                            <li>...</li>
+                            <li>...</li>
+                            <li>...</li>
+                            <li>...</li>
+                            <li>...</li>
+                            <li>...</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
     )
 }
