acf37a72d547a21ff855720981defe52a3e07b1d
Guilherme Braun <guiabraun@hotmail.com>
2022-09-21 09:53:12 -0300

    rename de pastas
    Rename de pastas dos components para melhor organização

--- components/Home/BemVindo/index.tsx
+++ components/Home/BemVindo/index.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link"
+import React from 'react'
 
-export const SectionOneHome = () => {
+
+export const BemVindo = () => {
     return (
-        <>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-32 gap-y-8 items-center justify-between container py-16">
+        <div className="bg-black min-h-screen bg-[url('/images/bg-section-one.png')] bg-cover bg-center flex">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-32 gap-y  -8 items-center justify-between container py-16">
                 <div className='flex flex-col gap-y-4 md:gap-y-8 text-center md:text-left justify-center items-center md:items-start'>
                     <h2 className="text-4xl">Bem vindo ao grupo Ousar</h2>
                     <p className='text-white'>Somos um ecossistema de núcleos especializados em
                         inteligência corporativa e marketing digital.</p>
                     <Link href="/quem-somos"><a className="bg-red p-4 rounded text-white w-fit">Quem Somos</a></Link>
                 </div>
-                <div>
                     <iframe className="w-full" height="315" src="https://www.youtube.com/embed/5a84tXRpBJI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                </div>
             </div>
-        </>
+        </div>
     )
 }
---
+++ components/Home/OQueFazemos/index.tsx
@@ -0,0 +1,20 @@
+import { sectorOusar } from '../../../utils/SectorsOusar'
+import { Sectors } from '../../Sectors'
+
+
+export const OQueFazemos = () => {
+    return (
+        <div className="bg-black min-h-screen py-8 flex justify-center items-center">
+            <div className="container gap-y-8 flex flex-col">
+                <h2 className="text-4xl text-center md:text-5xl text-3xl">O que Fazemos</h2>
+                <p className='text-white text-center md:text-2xl text-xl mx-auto'>Acompanhamos, identificamos e reagimos em tempo real aos fatores de disrupção tecnológica e social que afetam organizações mundo afora.</p>
+                <div className="grid md:grid-cols-3 grid-cols-1 gap-24 text-center justify-center items-center">
+                    {sectorOusar.map((item, index) => (
+                        <Sectors key={index} name={item.name} path={item.path} />
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    )
+}
