cac50913bde2a4b7b717b3ffafe960e845c0f9c7
Guilherme Braun <guiabraun@hotmail.com>
2022-09-21 10:09:25 -0300

    implementação de React Player

--- components/Home/BemVindo/index.tsx
+++ components/Home/BemVindo/index.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
 import React from 'react'
 
+import dynamic from 'next/dynamic';
+
+const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 export const BemVindo = () => {
     return (
@@ -12,7 +15,10 @@ export const BemVindo = () => {
                         inteligência corporativa e marketing digital.</p>
                     <Link href="/quem-somos"><a className="bg-red p-4 rounded text-white w-fit">Quem Somos</a></Link>
                 </div>
-                    <iframe className="w-full" height="315" src="https://www.youtube.com/embed/5a84tXRpBJI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <div className="max-w-full">
+                    <ReactPlayer url='https://www.youtube.com/watch?v=5a84tXRpBJI' width="auto"/>
+                </div>
+                    {/* <iframe className="w-full" height="315" src="https://www.youtube.com/embed/5a84tXRpBJI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
             </div>
         </div>
     )
