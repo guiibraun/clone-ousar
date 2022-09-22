4e12f9e79f64eb2c0a8b198c65f5c99b5a2fc80d
Guilherme Braun <guiabraun@hotmail.com>
2022-09-21 21:38:13 -0300

    Update index.tsx

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,7 +1,7 @@
 import { toast } from 'react-toastify';
 import { useForm } from 'react-hook-form';
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
+
+
 import 'react-toastify/dist/ReactToastify.css';
 import { InputError } from '../InputErrors'
 
@@ -13,15 +13,10 @@ type FormType = {
         message?: string
 }
 
-const validationSchema = yup.object({
-    name: yup.string().required(),
-    email: yup.string().required(),
-    phone: yup.string().required(),
-    message: yup.string()
-})
+
 
 export const Form = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm({resolver: yupResolver(validationSchema)}) 
+    const { register, handleSubmit, formState: {errors} } = useForm() 
 
     const onSubmit = (data: FormType) => {
         if(data.name !== '' && data.phone !== '' && data.phone !== ''){
@@ -53,17 +48,11 @@ export const Form = () => {
             <h4>Entre em contato conosco: </h4>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
                 <input type="text" placeholder='Seu nome' {...register('name')} className="py-2 px-4 text-black"/>
-                {errors?.name?.type && 
-                    <InputError error="nome" />
-                }
+
                 <input type="email" placeholder='Seu melhor e-mail' {...register('email')} className="py-2 px-4 text-black"/>
-                {errors?.email?.type &&
-                    <InputError error="e-mail" />
-                }
+   
                 <input type="text" placeholder='Seu melhor telefone' {...register('phone')} className="py-2 px-4 text-black"/>
-                {errors?.phone?.type &&
-                    <InputError error="telefone" />
-                }
+
                 <textarea className="py-2 px-4 text-black" rows={6} placeholder='Mensagem' {...register('message')}/>
                 <input type="submit" value="Enviar" className="bg-blackSubmit p-2"/>
             </form>
