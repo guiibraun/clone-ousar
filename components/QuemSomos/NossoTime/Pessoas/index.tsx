703271b42b438144e11affb33bb43082c894d205
Guilherme Braun <guiabraun@hotmail.com>
2022-09-22 21:21:29 -0300

    Update index.tsx

--- components/QuemSomos/NossoTime/Pessoas/index.tsx
+++ components/QuemSomos/NossoTime/Pessoas/index.tsx
@@ -8,7 +8,7 @@ export const Pessoas = () => {
             <div className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4">
                 {TimeOusar.map((item, index) => (
                     <div key={index} className={`${styles.timeContainer}`}>
-                        <Image src={item.image} height={362} width={261}  />
+                        <Image src={item.image} height={362} width={261}  alt=''/>
                         <div className={`${styles.timeInfo}`}>
                             <h2>{item.nome}</h2>
                             <h4>{item.cargo}</h4>
