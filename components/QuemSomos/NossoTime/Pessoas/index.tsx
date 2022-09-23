dce102fcd52f3f653604ecce174f4a459228bd9b
Guilherme Braun <guiabraun@hotmail.com>
2022-09-23 08:11:04 -0300

    Trabalhando no bg do Nosso Time

--- components/QuemSomos/NossoTime/Pessoas/index.tsx
+++ components/QuemSomos/NossoTime/Pessoas/index.tsx
@@ -5,7 +5,7 @@ import styles from '../../../../styles/TimeOusar.module.css'
 export const Pessoas = () => {
     return (
         <div className="pb-8 lg:py-16 bg-black">
-            <div className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
+            <div className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-2 max-w-screen-lg">
                 {TimeOusar.map((item, index) => (
                     <div key={index} className={`${styles.timeContainer}`}>
                         <Image src={item.image} height={362} width={261}  alt=''/>
