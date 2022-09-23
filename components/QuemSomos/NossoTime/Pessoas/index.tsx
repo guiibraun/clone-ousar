004d678769bad6349eace60090ba4cfb97e30dd7
Guilherme Braun <guiabraun@hotmail.com>
2022-09-23 07:53:48 -0300

    Mexendo no Time

--- components/QuemSomos/NossoTime/Pessoas/index.tsx
+++ components/QuemSomos/NossoTime/Pessoas/index.tsx
@@ -4,8 +4,8 @@ import styles from '../../../../styles/TimeOusar.module.css'
 
 export const Pessoas = () => {
     return (
-        <div className="py-16 bg-black">
-            <div className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4">
+        <div className="pb-8 lg:py-16 bg-black">
+            <div className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
                 {TimeOusar.map((item, index) => (
                     <div key={index} className={`${styles.timeContainer}`}>
                         <Image src={item.image} height={362} width={261}  alt=''/>
