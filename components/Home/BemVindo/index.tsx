6cd4d4aab2db1fc18151cd7597890505128fa67a
Guilherme Alexandre Braun <guiabraun@hotmail.com>
2022-10-04 10:37:50 -0300

    Update index.tsx

--- components/Home/BemVindo/index.tsx
+++ components/Home/BemVindo/index.tsx
@@ -14,7 +14,7 @@ export const BemVindo = () => {
                         inteligência corporativa e marketing digital.</p>
                     <Link href="/quem-somos"><a className="bg-red p-4 rounded text-white w-fit">Quem Somos</a></Link>
                 </div>
-                <ReactPlayer url='https://www.youtube.com/watch?v=5a84tXRpBJI' width="auto" height="300px" controls />
+                {/* <ReactPlayer url='https://www.youtube.com/watch?v=5a84tXRpBJI' width="auto" height="300px" controls /> */}
             </div>
         </div>
     )
