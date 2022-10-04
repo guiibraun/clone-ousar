e9eb302213a793d1c3517994e50054a2936eb72f
Guilherme Alexandre Braun <guiabraun@hotmail.com>
2022-10-04 09:47:27 -0300

    Mudanças

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -14,20 +14,20 @@ const validationSchema = yup.object({
 })
 
 type FormType = {
-        name?: string,
-        email?: string,
-        phone?: string,
-        message?: string
+    name?: string,
+    email?: string,
+    phone?: string,
+    message?: string
 }
 
 
 export const Form = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validationSchema)
-    }) 
+    })
 
     const onSubmit = (data: FormType) => {
-        if(data.name !== '' && data.phone !== '' && data.phone !== ''){
+        if (data.name !== '' && data.phone !== '' && data.phone !== '') {
             toast.success('Formulário enviado com sucesso', {
                 position: "top-right",
                 autoClose: 5000,
@@ -52,30 +52,31 @@ export const Form = () => {
     }
 
     return (
-<<<<<<< HEAD
-        <>
+        <div>
             <h4>Entre em contato conosco: </h4>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4" data-netlify="true">
-=======
-        <div className="h-full flex flex-col justify-center z-40 w-11/12 mx-auto">
-            <h4 className="text-xl mb-4 text-white">Entre em contato conosco: </h4>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
->>>>>>> ba614a05a58acd2568a73494794577faf53f5089
-                <input type="text" placeholder='Seu nome' {...register('name')} className="py-2 px-4 text-black"/>
-                {errors?.name?.type && 
-                    <InputError error="nome" />
-                }
-                <input type="email" placeholder='Seu melhor e-mail' {...register('email')} className="py-2 px-4 text-black"/>
-                {errors?.email?.type &&
-                    <InputError error="e-mail" />
-                }
-                <input type="text" placeholder='Seu melhor telefone' {...register('phone')} className="py-2 px-4 text-black"/>
-                {errors?.phone?.type &&
-                    <InputError error="telefone" />
-                }
-                <textarea className="py-2 px-4 text-black" rows={6} placeholder='Mensagem' {...register('message')}/>
-                <input type="submit" value="Enviar" className="bg-blackSubmit p-2 text-white"/>
+                <div className="h-full flex flex-col justify-center z-40 w-11/12 mx-auto">
+                    <h4 className="text-xl mb-4 text-white">Entre em contato conosco: </h4>
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+
+                        <input type="text" placeholder='Seu nome' {...register('name')} className="py-2 px-4 text-black" />
+                        {errors?.name?.type &&
+                            <InputError error="nome" />
+                        }
+                        <input type="email" placeholder='Seu melhor e-mail' {...register('email')} className="py-2 px-4 text-black" />
+                        {errors?.email?.type &&
+                            <InputError error="e-mail" />
+                        }
+                        <input type="text" placeholder='Seu melhor telefone' {...register('phone')} className="py-2 px-4 text-black" />
+                        {errors?.phone?.type &&
+                            <InputError error="telefone" />
+                        }
+                        <textarea className="py-2 px-4 text-black" rows={6} placeholder='Mensagem' {...register('message')} />
+                        <input type="submit" value="Enviar" className="bg-blackSubmit p-2 text-white" />
+                </div>
             </form>
         </div>
     )
+
+
 }
