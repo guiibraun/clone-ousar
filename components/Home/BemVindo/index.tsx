640b4b3354e4357fb118f0df0406ddd53b008d81
Guilherme Alexandre Braun <guiabraun@hotmail.com>
2022-10-05 11:33:41 -0300

    Update index.tsx

--- components/Home/BemVindo/index.tsx
+++ components/Home/BemVindo/index.tsx
@@ -11,7 +11,7 @@ export const BemVindo = () => {
                 <div className='flex flex-col gap-y-4 md:gap-y-8 text-center md:text-left justify-center items-center md:items-start'>
                     <h2 className="text-4xl">Bem vindo ao grupo Ousar</h2>
                     <p className='text-white'>Somos um ecossistema de núcleos especializados em
-                        inteligência corporativa e marketing digital.</p>
+                        inteligência corporativa e marketing digital</p>
                     <Link href="/quem-somos"><a className="bg-red-700 p-4 rounded text-white w-fitt text-bold">Quem Somos</a></Link>
                 </div>
                 {/* <ReactPlayer url='https://www.youtube.com/watch?v=5a84tXRpBJI' width="auto" height="300px" controls /> */}
